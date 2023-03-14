a44e3588a100ccb3af2f50409b943724f333bd22
Farhana Maha <32715719+mahafarhana@users.noreply.github.com>
2023-03-14 14:46:51 +0600

    Time: 44 ms (5.39%), Space: 43.1 MB (14.84%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int count;
+        int[] list=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+           
+             list[i]= count;
+             System.out.println(list);
+            
+        }
+        return list;
+    }
+}
