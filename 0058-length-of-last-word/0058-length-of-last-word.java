f97a9a2508d339b5bd01a88ab03b05a5d6def995
Farhana Maha <32715719+mahafarhana@users.noreply.github.com>
2023-04-13 14:18:02 +0600

    Time: 1 ms (45.85%), Space: 41.2 MB (11.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       
         String[] value=s.split(" ");
-        System.out.println(value[value.length-1].length());
        return value[value.length-1].length(); 
     }
 }
