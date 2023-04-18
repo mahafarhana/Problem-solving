689f0576322009f20c189b6ee6493c690eec9097
Farhana Maha <32715719+mahafarhana@users.noreply.github.com>
2023-04-18 15:31:50 +0600

    Time: 3 ms (97.96%), Space: 42.6 MB (72.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        boolean finalValue=false;
-        String reverseValue="";
-        String original=s.replaceAll("[^a-zA-Z^0-9]","").toLowerCase();
-        char[] c=original.toCharArray();
-        if(s.trim().equalsIgnoreCase("")){
-            return true;
-        }else if(c.length>0){
-            // int i=c.length-1;
-            //     do{
-            //          reverseValue+=String.valueOf(c[i]);
-            //         i--;
-            //     }
-            // while(i>=0);
-
-            for(int i=c.length-1;i>=0;i--){
-                
-                 reverseValue+=String.valueOf(c[i]);
-               
-            }
+       if (s.isEmpty()) {
+        	return true;
         }
-       
-        if(original.equalsIgnoreCase(reverseValue)){
-          finalValue=true;  
-        }else{
-             finalValue=false;  
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
         }
-        
-        return finalValue;
+        return true;
     }
 }
